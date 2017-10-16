6e55713ee364976212dc2df9be22e88875f568ed
George Wilson <gwilson@tcspeedshop.com>
2017-10-16 10:34:26 -0400

    fixed role_name value for CF deployment

--- account-perm-update.py
+++ account-perm-update.py
@@ -1,38 +1,36 @@
 import boto3
 import json
 import logging
-from datetime import datetime
-from dateutil.tz import tzutc
+
 from botocore.exceptions import ClientError
 
 log = logging.getLogger()
 log.setLevel(logging.DEBUG)
 
-#value added from Cloud Formation
-policyarn = '%POLICY_ARN%'
-policyarn = 'arn:aws:iam::228198000189:policy/TransitPollerXAccount'
+
+rolename = '%ROLE_NAME%'
+policyname= 'TransitPollerXAccount'
 
 
-#boto3.setup_default_session(profile_name='edc-transit')
 
 iam = boto3.client('iam')
+#add region_name value
 
 #Returns policy body for an inline policy in the role
-def get_managed_policy(policyarn):
+def get_managed_policy(rolename,policyname):
     try:
-        response = iam.get_policy(PolicyArn=policyarn)
+        response = iam.get_role_policy(RoleName=rolename,PolicyName=policyname)
 
     except ClientError, err:
-        #log.error(err)
-        print err
+        log.error(err)
 
-    policy = iam.get_policy_version(PolicyArn=response['Policy']['Arn'],VersionId=response['Policy']['DefaultVersionId'])
-    return json.dumps(policy['PolicyVersion']['Document'])
+    else:
+        log.info('Found policy %s',policyname)
+        return response['PolicyDocument']
 
 
 def iam_policy_list(policy):
     policy_list = list()
-    policy = json.loads(policy)
     for item in range(0,len(policy['Statement'])):
         policy_list.append(policy['Statement'][item]['Resource'].split(':')[4])
     return policy_list
@@ -72,11 +70,16 @@ def account_id_list():
 
 
 def remove_policy_element(policy,account_list):
-
+    list = []
     for acct in account_list:
-      for item in range(0, len(policy['Statement'])):
-        if acct in str(policy['Statement'][item]):
-          policy['Statement'].pop(item)
+        for item in range(len(policy['Statement'])):
+            if acct in str(policy['Statement'][item]):
+                print policy['Statement'][item]
+                list.append(item)
+    list.sort(reverse=True)
+    print list
+    for l in list:
+        policy['Statement'].pop(l)
     return policy
 
 
@@ -84,49 +87,21 @@ def add_policy_element(policy,account_list):
 
     for acct in account_list:
         policy['Statement'].append({u'Action': u'sts:AssumeRole',
-                                    u'Resource': u'arn:aws:iam::' + acct + ':role/TransitAccountPollerRole',
+                                    u'Resource': u'arn:aws:iam::' + acct + ':role/TransitXAccountPollerRole',
                                     u'Effect': u'Allow'})
     return policy
 
 
-def delete_old_policy_version(policyarn):
-    versions = iam.list_policy_versions(PolicyArn=policyarn)['Versions']
-    date = datetime.now(tzutc())
-    versionid = None
-
-    for ver in versions:
-        if ver['CreateDate'] <= date:
-            versionid = ver['VersionId']
-            date = ver['CreateDate']
-    print versionid, date
-
-
-    try:
-        iam.delete_policy_version(PolicyArn=policyarn,VersionId=versionid)
-
-    except ClientError, err:
-        print err.response['Error']['Message']
-        log.error('Error deleting policy version: %s Version ID: $s',policyarn.split('/')[1],versionid)
-        log.debug('Boto3 Exception attempting to delete policy version. %s %s',err.response['Error']['Code'],err.response['Error']['Message'])
-    else:
-        print 'deleted policy version'
-        log.info('Deleted policy version %s in Policy %s',versionid,policyarn.split('/')[1])
-
-
-def pushIamPolicy(policyarn, policy):
-    for attempt in range(3):
+def pushIamPolicy(rolename,policyname, policy):
         try:
-            iam.create_policy_version(PolicyArn=policyarn, PolicyDocument=policy,SetAsDefault=True)
+            iam.put_role_policy(RoleName=rolename, PolicyName=policyname, PolicyDocument=policy)
 
         except ClientError as err:
-            if err.response['Error']['Code'] == 'LimitExceeded':
-                delete_old_policy_version(policyarn)
-            else:
-                log.debug('Boto3 Exception attempting to add policy version. %s %s',err.response['Error']['Code'],err.response['Error']['Message'])
-                continue
+            print err
+            #log.debug('Boto3 Exception attempting to add policy version. %s', err)
+
         else:
-            log.info('Added new policy version Policy %s',policyarn.split('/')[1])
-            #print 'Added new policy version'
+            #log.info('Added new policy version to Role %s',rolename)
             return
 
 
@@ -134,7 +109,7 @@ def lambda_handler(event,context):
     policyUpdate = False
 
     #get managed policy details
-    iamPolicy = get_managed_policy(policyarn)
+    iamPolicy = get_managed_policy(rolename,policyname)
 
     #generate account list from current policy
     currentPolicyList = iam_policy_list(iamPolicy)
@@ -148,17 +123,23 @@ def lambda_handler(event,context):
     #determine which accounts need to deleted from the policy
     RemoveAccounts = list(set(currentPolicyList) - set(AccountList))
 
-    newIamPolicy = json.loads(iamPolicy)
+    newIamPolicy = iamPolicy
 
     if RemoveAccounts != []:
-      newIamPolicy = remove_policy_element(newIamPolicy,RemoveAccounts)
-      policyUpdate = True
+       # log.info('Accounts to be removed: %s', RemoveAccounts)
+        newIamPolicy = remove_policy_element(newIamPolicy,RemoveAccounts)
+        policyUpdate = True
 
     if AddAccounts != []:
-      newIamPolicy = add_policy_element(newIamPolicy,AddAccounts)
-      policyUpdate = True
+       # log.info('Accounts to be added: %s', AddAccounts)
+        newIamPolicy = add_policy_element(newIamPolicy,AddAccounts)
+        policyUpdate = True
 
     if policyUpdate:
-        pushIamPolicy(policyarn,json.dumps(newIamPolicy))
+        pushIamPolicy(rolename,policyname,json.dumps(newIamPolicy))
+        log.info('Policy %s has been updated',policyname)
+    else:
+         log.info('No Policy updates were needed at this time.')
+         return
 
 
